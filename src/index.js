ccbaef745d61d4fff6ba501efa187959bac82bee
Anastasiia Kichkimet <108686215+castelana@users.noreply.github.com>
2022-09-10 13:21:25 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,37 @@
+let now = new Date();
+let currentDate = document.querySelector("#currentDate");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+currentDate.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let units = "metric";
+  let city = searchInput.value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let form = document.querySelector(".search-form");
+form.addEventListener("submit", search);
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${temperature}°C`;
+}
